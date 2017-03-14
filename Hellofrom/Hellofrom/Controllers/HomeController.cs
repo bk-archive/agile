56c25145af6388cb11c836de1f5f1836eb598d6d
Byron Tardif <byvinyal@microsoft.com>
2017-03-14 16:15:24 -0700

    Adding some bugs

--- Hellofrom/Hellofrom/Controllers/HomeController.cs
+++ Hellofrom/Hellofrom/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,6 +11,40 @@ namespace Hellofrom.Controllers
     {
         public ActionResult Index()
         {
+            Random coin = new Random();
+
+            int cointoss = coin.Next(0, 9);
+
+            switch (cointoss)
+            {
+                case 0:
+                    // I have a bug where the page is slow....
+                    Thread.Sleep(2000);
+                    break;
+                case 1:
+                    // I have a bug where the page is slow....
+                    Thread.Sleep(2000);
+                    break;
+                case 2:
+                    // I have a bug where the page is slow....
+                    Thread.Sleep(2000);
+                    break;
+                case 3:
+                    // I have a bug where the page throws 500
+                    throw new Exception("Cointoss = " + cointoss);
+                    break;
+                case 4:
+                    // I have a bug where the page throws 500
+                    throw new Exception("Cointoss = 1" + cointoss);
+                    break;
+
+                default:
+                    //if the value is >= 5 then tere is no bug :)
+                    break;
+            }
+
+
+
             return View();
         }
     }
